649163c87b519683454790db654758a393e3efe5
Davi-yyy <davismoraes.0001@gmail.com>
2022-02-04 20:46:55 -0300

    Fix Login function 🛠

--- src/services/login.service.ts
+++ src/services/login.service.ts
@@ -2,19 +2,23 @@ import User from "../entity/User";
 import { CustomError } from "../utils/error";
 import { Service } from "typedi";
 import { Tokenfy } from "../utils/jwt";
+import { compare } from "bcrypt";
 
 @Service()
 export default class LoginService {
 
     findByLogin = async (data: { email: string, password: string }): Promise<User | CustomError | { user: User, token: string }> => {
-
-        const result = await User.findOne({ where: { email: data.email, password: data.password } });
+        const result = await User.findOne({ where: { email: data.email } });
 
         if (result) {
-            const jwt = new Tokenfy();
-            const token = await jwt.generateToken(result);
-            result['password'] = 'secret';
-            return  { user: result, token: token }
+            
+            if ( await compare(data.password, result.password)) {
+
+                const jwt = new Tokenfy();
+                const token = await jwt.generateToken(result);
+                result['password'] = 'secret';
+                return { user: result, token: token }
+            }
         }
         return {
             error: true,
@@ -24,5 +28,5 @@ export default class LoginService {
         }
 
     }
-    
+
 }
